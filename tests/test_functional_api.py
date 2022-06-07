2aa9828a7f968584cef995879bc10956cf81abee
afro-juliano <afro.juliano@gmail.com>
2022-06-07 01:43:31 -0300

    test_api

---
+++ tests/test_functional_api.py
@@ -0,0 +1,22 @@
+from fastapi.testClient import TestClient
+
+from beerlog.api import api
+
+client = TestClient(api)
+
+
+def test_create_beer_via_api():
+    response = client.post(
+        "/beers",
+        json={
+            "name": "Skol",
+            "style": "KornPA",
+            "flavour": 1,
+            "image": 1,
+            "cost": 2,
+        },
+    )
+    assert response.status_code == 201
+    result = response.json()
+    assert result["name"] == "Skol"
+    assert result["id"] == 1
